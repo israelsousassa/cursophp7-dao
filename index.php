b15b0d6ddb69773fe2d2ebdeeadbece44cc71713
israelsousassa <isousa2806@gmail.com>
2020-12-23 10:02:29 -0300

    metodos  de lista

--- index.php
+++ index.php
@@ -4,11 +4,31 @@
 
 require_once("config.php");
 
+/* carrega um usuário
 $userr = new usuarios();
 
 $userr->loadbyId(3);
 
-echo $userr;
+echo $userr; */
+
+/*carrega uma lista de usuários
+
+$lista = usuarios::getList();
+
+echo json_encode($lista);*/
+
+/*carrega uma lista de usuários buscando pelo login
+
+$search = usuarios::search("la");
+
+echo json_encode($search);*/
+
+// carrega um usuário usando o login e a senha
+
+$usuario = new usuarios();
+$usuario->login("lara23","$%yth&12");
+
+echo $usuario;
 
 
 
